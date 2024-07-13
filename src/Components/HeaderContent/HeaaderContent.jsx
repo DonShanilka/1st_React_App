2c55e188e5f77be2daf0f7cc87ce8efce4cc5f6c
Shanilka <shanilka799@gmail.com>
2024-07-13 23:29:19 +0530

    use image

--- src/Components/HeaderContent/HeaaderContent.jsx
+++ src/Components/HeaderContent/HeaaderContent.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import MenuLink from '../../MenuLink/MenuLink'
 import './Header.css'
+import LogoImage from '../../assets/react.svg'
 
 function HeaaderContent() {
     return (
         <div id='navcontent'>
             <a href="http://www.react.dev"></a>
+
             <div>
+                <img src={LogoImage} alt="" />
                 <MenuLink linkname="Home" url="#home" />
                 <MenuLink linkname="About" url="#about" />
                 <MenuLink linkname="Contact" url="#contact" />
                 <MenuLink linkname="Login" url="#login" />
+                <img src="vite.svg" alt="Vite Logo" />
             </div>
         </div>
     )
